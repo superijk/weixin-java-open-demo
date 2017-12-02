72e5837145a40aac701c492bc19a83746c20dcbc
007 <007gzs@gmail.com>
2017-12-02 16:29:32 +0800

    增加全网发布测试用例，完善readme

--- src/main/java/com/github/binarywang/demo/wechat/controller/WechatNotifyController.java
+++ src/main/java/com/github/binarywang/demo/wechat/controller/WechatNotifyController.java
@@ -2,13 +2,16 @@ package com.github.binarywang.demo.wechat.controller;
 
 import com.github.binarywang.demo.wechat.service.WxOpenServiceDemo;
 import me.chanjar.weixin.common.exception.WxErrorException;
+import me.chanjar.weixin.mp.bean.kefu.WxMpKefuMessage;
 import me.chanjar.weixin.mp.bean.message.WxMpXmlMessage;
+import me.chanjar.weixin.mp.bean.message.WxMpXmlOutMessage;
 import me.chanjar.weixin.open.bean.message.WxOpenXmlMessage;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
 /**
  * @author <a href="https://github.com/007gzs">007</a>
  */
@@ -18,6 +21,7 @@ public class WechatNotifyController {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     protected WxOpenServiceDemo wxOpenService;
+
     @RequestMapping("receive_ticket")
     public Object receiveTicket(@RequestBody(required = false) String requestBody, @RequestParam("timestamp") String timestamp,
                                 @RequestParam("nonce") String nonce, @RequestParam("signature") String signature,
@@ -46,8 +50,9 @@ public class WechatNotifyController {
 
         return out;
     }
+
     @RequestMapping("{appId}/callback")
-    public Object callback(@RequestBody(required = false)String requestBody,
+    public Object callback(@RequestBody(required = false) String requestBody,
                            @PathVariable("appId") String appId,
                            @RequestParam("signature") String signature,
                            @RequestParam("timestamp") String timestamp,
@@ -67,7 +72,31 @@ public class WechatNotifyController {
         // aes加密的消息
         WxMpXmlMessage inMessage = WxOpenXmlMessage.fromEncryptedMpXml(requestBody, wxOpenService.getWxOpenConfigStorage(), timestamp, nonce, msgSignature);
         this.logger.debug("\n消息解密后内容为：\n{} ", inMessage.toString());
-        //wxOpenService.getWxOpenComponentService().getWxMpServiceByAppid(appId);
+        // 全网发布测试用例
+        if (StringUtils.equalsAnyIgnoreCase(appId, "wxd101a85aa106f53e", "wx570bc396a51b8ff8")) {
+            try {
+                if (StringUtils.equals(inMessage.getMsgType(), "text")) {
+                    if (StringUtils.equals(inMessage.getContent(), "TESTCOMPONENT_MSG_TYPE_TEXT")) {
+                        out = WxOpenXmlMessage.wxMpOutXmlMessageToEncryptedXml(
+                                WxMpXmlOutMessage.TEXT().content("TESTCOMPONENT_MSG_TYPE_TEXT_callback")
+                                        .fromUser(inMessage.getToUser())
+                                        .toUser(inMessage.getFromUser())
+                                        .build(),
+                                wxOpenService.getWxOpenConfigStorage()
+                        );
+                    } else if (StringUtils.startsWith(inMessage.getContent(), "QUERY_AUTH_CODE:")) {
+                        String msg = inMessage.getContent().replace("QUERY_AUTH_CODE:", "") + "_from_api";
+                        WxMpKefuMessage kefuMessage = WxMpKefuMessage.TEXT().content(msg).toUser(inMessage.getFromUser()).build();
+                        wxOpenService.getWxOpenComponentService().getWxMpServiceByAppid(appId).getKefuService().sendKefuMessage(kefuMessage);
+                    }
+                } else if (StringUtils.equals(inMessage.getMsgType(), "event")) {
+                    WxMpKefuMessage kefuMessage = WxMpKefuMessage.TEXT().content(inMessage.getEvent() + "from_callback").toUser(inMessage.getFromUser()).build();
+                    wxOpenService.getWxOpenComponentService().getWxMpServiceByAppid(appId).getKefuService().sendKefuMessage(kefuMessage);
+                }
+            } catch (WxErrorException e) {
+                logger.error("callback", e);
+            }
+        }
         return out;
     }
 }
